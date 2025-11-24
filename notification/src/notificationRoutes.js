e8b0bf5af4974436cb76554b0888dfbd23722828
Nícolas <126731327+NicolasCorneli@users.noreply.github.com>
2025-11-24 02:07:37 -0300

    Delete notification directory

--- notification/src/notificationRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { sendNotification } = require("./notificationController");
-
-const router = express.Router();
-
-router.post("/", sendNotification);
-
-module.exports = router;
